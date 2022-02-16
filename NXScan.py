e0533e46949ba1b85e3814691ff4bff6b3511c66
g147 <26509147+g147@users.noreply.github.com>
2022-02-16 13:51:10 +0530

    NXScan[2.0]

--- NXScan.py
+++ NXScan.py
@@ -16,7 +16,7 @@ GREEN = '\033[92m'
 YELLOW = '\033[93m'
 CLEAR = '\x1b[0m'
 
-print(BLUE + "NXScan[1.9] by ARPSyndicate" + CLEAR)
+print(BLUE + "NXScan[2.0] by ARPSyndicate" + CLEAR)
 print(YELLOW + "fast port scanning with fancy output" + CLEAR)
 
 if len(sys.argv) < 2:
@@ -38,19 +38,21 @@ else:
     parser.add_option('-r', '--retries', action="store", dest="retries",
                       help="number of times to enumerate [default= 1]", default=1)
     parser.add_option('--only-enumerate', action="store_false", dest="scan",
-                      help="only enumerate open ports using naabu", default=True)
+                      help="only enumerate open ports using naabu & shodan", default=True)
     parser.add_option('--only-finger', action="store_true", dest="finger",
                       help="only fingerprint services using nmap", default=False)
     parser.add_option('--only-scan', action="store_false",
-                      dest="enumerate", help="only scan using nmap", default=True)
-    parser.add_option('--only-shodan', action="store_true",
-                      dest="shodan", help="only scan using shodan", default=False)
+                      dest="enumerate", help="only scan using nmap & shodan", default=True)
+    parser.add_option('--only-shodan-enum', action="store_true",
+                      dest="shodane", help="only enum using shodan", default=False)
+    parser.add_option('--only-shodan-scan', action="store_true",
+                      dest="shodans", help="only scan using shodan", default=False)
     parser.add_option('--ports', action="store", dest="ports",
-                      help="top-100,top-1000,full [default=top-1000]", default="-Pn -A -T5")
+                      help="top-100,top-1000,full [default=top-1000]", default="top-1000")
     parser.add_option('--nmap-param', action="store", dest="nmpara",
                       help="nmap parameters [default= -Pn -A -T5]", default="-Pn -A -T5")
     parser.add_option('--naabu-param', action="store", dest="napara",
-                      help="naabu parameters [default= -rate 500 -timeout 2500 -stats -retries 3 -scan-all-ips -exclude-cdn]", default="-rate 500 -timeout 2500 -stats -retries 3 -scan-all-ips -exclude-cdn ")
+                      help="naabu parameters [default= -rate 800 -timeout 2500 -stats -retries 2 -scan-all-ips -exclude-cdn]", default="-rate 800 -timeout 2500 -stats -retries 2 -scan-all-ips -exclude-cdn ")
 
 inputs, args = parser.parse_args()
 if not inputs.list:
@@ -64,15 +66,21 @@ threads = int(inputs.threads)
 enum = inputs.enumerate
 scan = inputs.scan
 finger = inputs.finger
-shodan = inputs.shodan
+shodane = inputs.shodane
+shodans = inputs.shodans
 template = str(inputs.template)
 napara = "-top-ports {0} {1}".format(inputs.ports, inputs.napara)
 nmpara = inputs.nmpara
 retries = int(inputs.retries)
 
-if finger or shodan:
+if finger or shodane or shodans:
     scan = False
     enum = False
+
+if scan:
+    shodans = True
+if enum:
+    shodane = True
 
 if(os.path.exists(output) == False):
     os.system("mkdir {0}".format(output))
@@ -83,16 +91,35 @@ if(os.path.exists(list) == False or os.stat(list).st_size == 0):
 if threads > 3:
     threads = 3
 
-shodanresult = []
+shodansresult = []
 def shodanScan(target):
-    global shodanresult
-    sdat = requests.get("https://internetdb.shodan.io/"+socket.gethostbyname(target)).json()
-    shodanresult.append("{0} {1} {2} {3} {4} {5} {6}".format(target, sdat['ip'], sdat['ports'], sdat['cpes'], sdat['hostnames'], sdat['tags'], sdat['vulns']))
+    global shodansresult
     if verbose:
         print(GREEN + "[VERBOSE] started scanning {0}".format(target) + CLEAR)
+    try:
+        sdat = requests.get("https://internetdb.shodan.io/"+socket.gethostbyname(target)).json()
+    except:
+        return
+    shodansresult.append("{0} {1} {2} {3} {4} {5} {6}".format(target, sdat['ip'], sdat['ports'], sdat['cpes'], sdat['hostnames'], sdat['tags'], sdat['vulns']))
     print(BLUE + "[+] completed scanning {0}".format(target) + CLEAR)
-    with open("{0}/shodan.txt".format(output), "w") as f:
-        f.writelines("%s\n" % line for line in shodanresult)
+    with open("{0}/shodan-scan.txt".format(output), "w") as f:
+        f.writelines("%s\n" % line for line in shodansresult)
+
+shodaneresult = []
+def shodanEnum(target):
+    global shodaneresult
+    if verbose:
+        print(GREEN + "[VERBOSE] started enumerating {0}".format(target) + CLEAR)
+    try:
+        sdat = requests.get("https://internetdb.shodan.io/"+socket.gethostbyname(target)).json()
+    except:
+        return
+    for port in sdat['ports']:
+        shodaneresult.append("{0}:{1}".format(target, port))
+    print(BLUE + "[+] completed enumerating {0}".format(target) + CLEAR)
+    with open("{0}/shodan-enum.txt".format(output), "w") as f:
+        f.writelines("%s\n" % line for line in shodaneresult)
+
 
 def nmapScan(target):
     host = target.split(" ")[0]
@@ -179,19 +206,20 @@ def generateHTML():
                                                "/scan.html", template, final))
     return
 
-
-if enum:
-    print(YELLOW + "[*] enumerating using naabu" + CLEAR)
-    for i in range(0, retries):
-        os.system(
-            "sudo naabu -l {0} -s s -o {2}/enum-syn-{3}.txt {1}".format(list, napara, output, i))
-        os.system(
-            "sudo naabu -l {0} -s c -o {2}/enum-con-{3}.txt {1}".format(list, napara, output, i))
-    os.system(
-        "cat {0}/enum-syn-*.txt {0}/enum-con-*.txt | sort -u > {0}/enum.txt".format(output))
-    list = "{0}/enum.txt".format(output)
-
-if shodan:
+if shodane:
+    print(YELLOW + "[*] enumerating using shodan" + CLEAR)
+    with open(list) as f:
+        domains = f.read().splitlines()
+    with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+        try:
+            executor.map(shodanEnum, domains)
+        except(KeyboardInterrupt, SystemExit):
+            print(RED + "[!] interrupted" + CLEAR)
+            executor.shutdown(wait=False)
+            sys.exit()
+    os.system("cat {0}/enum-syn-*.txt {0}/enum-con-*.txt {0}/shodan-enum.txt | sort -u > {0}/enum.txt".format(output))    
+    
+if shodans:
     print(YELLOW + "[*] scanning using shodan" + CLEAR)
     with open(list) as f:
         domains = f.read().splitlines()
@@ -202,6 +230,19 @@ if shodan:
             print(RED + "[!] interrupted" + CLEAR)
             executor.shutdown(wait=False)
             sys.exit()
+
+
+if enum:
+    print(YELLOW + "[*] enumerating using naabu" + CLEAR)
+    for i in range(0, retries):
+        os.system(
+            "sudo naabu -l {0} -s s -o {2}/enum-syn-{3}.txt {1}".format(list, napara, output, i))
+        os.system(
+            "sudo naabu -l {0} -s c -o {2}/enum-con-{3}.txt {1}".format(list, napara, output, i))
+    os.system(
+        "cat {0}/enum-syn-*.txt {0}/enum-con-*.txt {0}/shodan-enum.txt | sort -u > {0}/enum.txt".format(output))
+    list = "{0}/enum.txt".format(output)
+
 
 if finger:
     print(YELLOW + "[*] fingerprinting using nmap" + CLEAR)
--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 ## Instructions for running
 1. Make sure `nmap`, `naabu` & `xsltproc` are installed.<br>
 2. Run `python3 NXScan.py`. <br>
-3. `naabu` + `nmap` + `xsltproc` == `NXScan`
+3. `shodan` + `naabu` + `nmap` + `xsltproc` == `NXScan`
 
 **COMPATIBILITY TESTED ON ARCHLINUX(x64) & DEBIAN(x64) ONLY**<br>
 **FEEL FREE TO SUBMIT PULL REQUESTS**
